caaad66a292a904c22e4256113085c9d2337890f
nbwilson <nbwilson@email.wm.edu>
2021-06-05 12:20:36 -0400

    Release 0.4
    Implemented move(). Created two helper methods, isMovePossible() and
    swap(). Passes testGetPathLength, testGetOperation, testGetParent1,
    testGetParent, testGetOperation1, and testGetPathLength1.

--- SlidingPuzzleByNicholasWilson/src/edu/wm/cs/cs301/slidingpuzzle/SimplePuzzleState.java
+++ SlidingPuzzleByNicholasWilson/src/edu/wm/cs/cs301/slidingpuzzle/SimplePuzzleState.java
@@ -6,23 +6,26 @@ public class SimplePuzzleState implements PuzzleState {
 	private int[][] board;
 	private Operation operation;
 	private int pathLength;
+	private int dim;
 	
 	public SimplePuzzleState() {
 		
 	}
 	
-	private SimplePuzzleState(PuzzleState p, int[][] b, Operation op, int pL) {//Private Constructor that lets you create a new SimplePuzzleState with set fields
+	private SimplePuzzleState(PuzzleState p, int[][] b, Operation op, int pL, int d) {//Private Constructor that lets you create a new SimplePuzzleState with set fields
 		// TODO Auto-generated constructor stub
 		parent = p;
 		board = b;
 		operation = op;
 		pathLength = pL;
+		dim = d;
 	}
 	
 	@Override
 	public void setToInitialState(int dimension, int numberOfEmptySlots) {
 		// TODO Auto-generated method stub
 		pathLength = 0;
+		dim = dimension;
 		board = new int[dimension][dimension];
 		for(int row = 0; row < dimension; row++) {
 			for(int col = 0; col < dimension; col++) {
@@ -64,6 +67,22 @@ public class SimplePuzzleState implements PuzzleState {
 	@Override
 	public PuzzleState move(int row, int column, Operation op) {
 		// TODO Auto-generated method stub
+		if (isMovePossible(row, column, op)) { //Is the move possible?
+			int[][] newBoard; 
+			switch(op) {
+				case MOVELEFT: //In each operation:
+					newBoard = swap(row, column, row, column - 1); //Create a new board with the values swapped
+				case MOVERIGHT:
+					newBoard = swap(row, column, row, column + 1);
+				case MOVEUP:
+					newBoard = swap(row, column, row - 1, column);
+				case MOVEDOWN:
+					newBoard = swap(row, column, row + 1, column);
+				default:
+					newBoard = board;
+			}
+			return new SimplePuzzleState(this, newBoard, op, pathLength + 1, dim); //Create and return a new SimplePuzzleState
+		}
 		return null;
 	}
 
@@ -90,5 +109,39 @@ public class SimplePuzzleState implements PuzzleState {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	private boolean isMovePossible(int row, int col, Operation op) { //Tells you if the move is possible
+		switch(op) { //Different calculations for different moves
+			case MOVERIGHT:
+				if(col == dim - 1)
+					return false;
+				else
+					return(board[row][col + 1] == 0);
+			case MOVELEFT:
+				if(col == 0)
+					return false;
+				else
+					return(board[row][col - 1] == 0);
+			case MOVEDOWN:
+				if(row == dim - 1)
+					return false;
+				else
+					return(board[row + 1][col] == 0);
+			case MOVEUP:
+				if(row == 0)
+					return false;
+				else
+					return(board[row - 1][col] == 0);
+			default:
+				return false;
+		}
+	}
+	
+	private int[][] swap(int r1, int c1, int r2, int c2){ //Swaps two values and returns a new array with the values swapped
+		int[][] newBoard = board.clone();
+		int val1 = newBoard[r1][c1];
+		newBoard[r1][c1] = newBoard[r2][c2];
+		newBoard[r2][c2]= val1; 
+		return newBoard;
+	}
 }
