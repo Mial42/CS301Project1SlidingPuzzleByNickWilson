a31638b52c8b38a4c1238e5f04c1ed84889ac867
nbwilson <nbwilson@email.wm.edu>
2021-06-05 10:03:26 -0400

    Release 0.2
    This version implements isEmpty(int row, int column), setToInitialState,
    and getValue. It passes testSetToInitialStateIntIntSeveralSlots and
    testSetToInitialStateIntIntOneSlot and testIsEmpty.

--- SlidingPuzzleByNicholasWilson/src/edu/wm/cs/cs301/slidingpuzzle/SimplePuzzleState.java
+++ SlidingPuzzleByNicholasWilson/src/edu/wm/cs/cs301/slidingpuzzle/SimplePuzzleState.java
@@ -2,16 +2,30 @@ package edu.wm.cs.cs301.slidingpuzzle;
 
 public class SimplePuzzleState implements PuzzleState {
 
+	private PuzzleState parent;
+	private int[][] board;
+	
 	@Override
 	public void setToInitialState(int dimension, int numberOfEmptySlots) {
 		// TODO Auto-generated method stub
-
+		board = new int[dimension][dimension];
+		for(int row = 0; row < dimension; row++) {
+			for(int col = 0; col < dimension; col++) {
+				int linearIndex = row * dimension + col;
+				if(dimension * dimension - linearIndex - numberOfEmptySlots > 0) {
+					board[row][col] = linearIndex + 1;
+				}
+				else {
+					board[row][col] = 0;
+				}
+			}
+		}
 	}
 
 	@Override
 	public int getValue(int row, int column) {
 		// TODO Auto-generated method stub
-		return 0;
+		return board[row][column];
 	}
 
 	@Override
@@ -53,7 +67,7 @@ public class SimplePuzzleState implements PuzzleState {
 	@Override
 	public boolean isEmpty(int row, int column) {
 		// TODO Auto-generated method stub
-		return false;
+		return(board[row][column]==0);
 	}
 
 	@Override
